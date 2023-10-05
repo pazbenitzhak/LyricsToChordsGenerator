7ac72349d65b99815459335246ccd734725057d2
amittal2001 <73400261+amittal2001@users.noreply.github.com>
2023-10-05 14:39:00 +0300

    Add files via upload

--- BERT_For_Chords.py
+++ BERT_For_Chords.py
@@ -1,10 +1,7 @@
 import torch.nn as nn
 
 class BERT_For_Chords(nn.Module):
-    def __init__(self,
-                 bert,
-                 output_dim,
-                 dropout):
+    def __init__(self, bert, output_dim, dropout=0, dim=1, hidden_layer_size=300, activation=nn.ReLU()):
 
         super().__init__()
 
@@ -12,7 +9,11 @@ class BERT_For_Chords(nn.Module):
 
         embedding_dim = bert.config.to_dict()['hidden_size']
 
-        self.fc = nn.Linear(embedding_dim, output_dim)
+        self.fc1 =  nn.Sequential(nn.Linear(embedding_dim, hidden_layer_size), activation)
+
+        self.hidden_layers = nn.Sequential(*[nn.Linear(hidden_layer_size, hidden_layer_size), activation]*dim)
+
+        self.fc2 = nn.Linear(hidden_layer_size, output_dim)
 
         self.dropout = nn.Dropout(dropout)
 
@@ -32,7 +33,11 @@ class BERT_For_Chords(nn.Module):
 
         #embedded = [sent len, batch size, emb dim]
 
-        predictions = self.fc(self.dropout(embedded))
+        predictions = self.fc1(self.dropout(embedded))
+
+        predictions = self.hidden_layers(self.dropout(predictions))
+
+        predictions = self.fc2(self.dropout(predictions))
 
         #predictions = [sent len, batch size, output dim]
 
