9b6adb18552e3251404e3c82444d7d71a301d8a5
agadish <agadish@gmail.com>
2023-10-22 20:15:40 +0300

    Decreased RepetitionCritea ideallcity for speed

--- chord_critic.py
+++ chord_critic.py
@@ -118,8 +118,6 @@ class ChordsProgressionsCoverageCriteria(ChordsMatchCriteria):
         score = sum(best_used_map) / len(functions)
         return score
     
-
-    
     def _calc_score_subset(self, functions, progressions, functions_used_maps):
         best_used_map = functions_used_maps
         if len(functions) <= 1:
@@ -138,111 +136,117 @@ class ChordsProgressionsCoverageCriteria(ChordsMatchCriteria):
 
 class RepetitionCriteria(ChordsMatchCriteria):
     MIN_REPETITION = 4
-    def _find_repetition_mask(self, occupied_mask, tested_pairs):
-        best_repetition_mask = occupied_mask
-        for l in range(self.MIN_REPETITION, len(self.chords) // 2):
-            for i in range(0, len(self.chords) - self.MIN_REPETITION + 1):
-                reps_indexes = [i for i in find_all_sublist(self.chords, self.chords[i : i + l])
-                                if not any(occupied_mask[i : i + l]) and not (l, i, ) in tested_pairs]
-                if len(reps_indexes) <= 1:
-                    continue
+    def _find_repetition_mask(self):
+        tested_pairs = []
+        occupied_mask = [False] * len(self.chords)
+        l = self.MIN_REPETITION
+        for i in range(len(self.chords) - l * 2 + 1):
+            reps_indexes = [i for i in find_all_sublist(self.chords, self.chords[i : i + l])
+                            if (l, i, ) not in tested_pairs]
+            if len(reps_indexes) <= 1:
+                continue
 
-                cloned_mask = list(occupied_mask) # clone
-                for i in reps_indexes:
-                    cloned_mask = cloned_mask[: i] + [True] * l + cloned_mask[i + l :]
-                current_mask = self._find_repetition_mask(cloned_mask, tested_pairs + [(l, i, )])
-                if sum(current_mask) > sum(best_repetition_mask):
-                    best_repetition_mask = current_mask
+            for i in reps_indexes:
+                occupied_mask = occupied_mask[: i] + [True] * l + occupied_mask[i + l :]
+            # new_submask = self._find_repetition_mask(chords, cloned_mask, tested_pairs + [(l, i, ) for i in reps_indexes])
+            # if sum(new_submask) > sum(best_repetition_mask):
+                # best_repetition_mask = new_submask
         
-        return best_repetition_mask
+        return occupied_mask
     
     def calc_score(self):
-        best_mask = self._find_repetition_mask([False] * len(self.chords), [])
+        best_mask = self._find_repetition_mask()
         score = sum(best_mask) / len(self.chords)
         return score
     
-class ScaleMatch(object):
-    def __init__(self, scale, chords, cp, criterias=None):
-        if not criterias:
-            criterias = list()
-        self.criterias = [
-            DiatomicMatchCriteria(scale, chords, cp),
-            FirstChordMatchCriteria(scale, chords, cp),
-            LastChordMatchCriteria(scale, chords, cp),
-            ChordsProgressionsCoverageCriteria(scale, chords, cp),
-            RepetitionCriteria(scale, chords, cp)
-        ]
-        self.scale = scale
-        self.chords = chords
-        self.cp = cp
-        self.score = sum([criteria.score for criteria in self.criterias])
+class ScaleMatchCriteria(ChordsMatchCriteria):
+    CRITERIAS_CLASSES = [DiatomicMatchCriteria, FirstChordMatchCriteria, LastChordMatchCriteria, ChordsProgressionsCoverageCriteria]
+    def calc_score(self):
+        critereas_scores = [criteria_cls(self.scale, self.chords, self.cp).score
+                            for criteria_cls in self.CRITERIAS_CLASSES]
+        return sum(critereas_scores)
+    
+# class ScaleMatch(object):
+#     def __init__(self, scale, chords, cp, criterias=None):
+#         if not criterias:
+#             criterias = list()
+#         self.criterias = [
+#             RepetitionCriteria(scale, chords, cp)
+#         ]
+#         self.scale = scale
+#         self.chords = chords
+#         self.cp = cp
+#         self.score = sum([criteria.score for criteria in self.criterias])
 
-    def __lt__(self, other):
-        if self.score != other.score:
-            return self.score < other.score
+#     def __lt__(self, other):
+#         if self.score != other.score:
+#             return self.score < other.score
         
-        if self.chords[0].root != self.scale.root:
-            if self.chords[0].root == self.chords[0].root:
-                return False
-            elif self.scale.root == self.chords[0].root:
-                return True
+#         if self.chords[0].root != self.scale.root:
+#             if self.chords[0].root == self.chords[0].root:
+#                 return False
+#             elif self.scale.root == self.chords[0].root:
+#                 return True
    
-        # Equal
-        return False
+#         # Equal
+#         return False
     
-    def __le__(self, other):
-        if self.score != other.score:
-            return self.score <= other.score
+#     def __le__(self, other):
+#         if self.score != other.score:
+#             return self.score <= other.score
         
-        if self.chords[0].root != self.scale.root:
-            if self.chords[0].root == self.chords[0].root:
-                return False
-            elif self.scale.root == self.chords[0].root:
-                return True
+#         if self.chords[0].root != self.scale.root:
+#             if self.chords[0].root == self.chords[0].root:
+#                 return False
+#             elif self.scale.root == self.chords[0].root:
+#                 return True
    
-        # Equal
-        return True
+#         # Equal
+#         return True
         
-    def __gt__(self, other):
-        if self.score != other.score:
-            return self.score > other.score
+#     def __gt__(self, other):
+#         if self.score != other.score:
+#             return self.score > other.score
         
-        if self.chords[0].root != self.scale.key.root:
-            if self.chords[0].root == self.chords[0].root:
-                return True
-            elif self.scale.key.root == self.chords[0].root:
-                return False
+#         if self.chords[0].root != self.scale.key.root:
+#             if self.chords[0].root == self.chords[0].root:
+#                 return True
+#             elif self.scale.key.root == self.chords[0].root:
+#                 return False
    
-        # Equal
-        return False
+#         # Equal
+#         return False
         
-    def __ge__(self, other):
-        if self.score != other.score:
-            return self.score > other.score
+#     def __ge__(self, other):
+#         if self.score != other.score:
+#             return self.score > other.score
         
-        if self.chords[0].root != self.scale.root:
-            if self.chords[0].root == self.chords[0].root:
-                return True
-            elif self.scale.root == self.chords[0].root:
-                return False
+#         if self.chords[0].root != self.scale.root:
+#             if self.chords[0].root == self.chords[0].root:
+#                 return True
+#             elif self.scale.root == self.chords[0].root:
+#                 return False
    
-        # Equal
-        return True
+#         # Equal
+#         return True
     
-    def __cmp__(self, other):
-        return self.scale == other._scale and self.chords == other._chords
+#     def __cmp__(self, other):
+#         return self.scale == other._scale and self.chords == other._chords
     
-    def __str__(self):
-        return f'[ScaleMatch: {self.scale} score={self.score}]'
+#     def __str__(self):
+#         return f'[ScaleMatch: {self.scale} score={self.score}]'
     
-    def __repr__(self):
-        return self.__str__()
+#     def __repr__(self):
+#         return self.__str__()
 
 class SongsChords(object):
+    CRITERIA_CLASSES = [
+        RepetitionCriteria
+    ]
     def __init__(self, chords: list):
         self.cp = chordparser.Parser()
         self._dedup_raw_chords = self._remove_dupliate_chords(chords)
-        self._chords = [self.cp.create_chord(c) for c in self._dedup_raw_chords]
+        self.chords = [self.cp.create_chord(c) for c in self._dedup_raw_chords]
 
     @classmethod
     def _remove_dupliate_chords(cls, chords):
@@ -252,21 +256,27 @@ class SongsChords(object):
                 new_chords.append(chord)
         return new_chords
 
-    def find_best_scale(self):
+    def _find_best_scale(self):
         # The scale will be major/minor depending on the type of the first note
         best_score = 0
         best_scale = None
-        scale_type = self._chords[0].quality.value # 'major' or 'minor'
-        all_scales = [ScaleMatch(self.cp.create_scale(root, scale_type), self._chords, self.cp) for root in NOTE_TO_TOKEN.keys()]
-        best_scale = max(all_scales)
+        scale_type = self.chords[0].quality.value # 'major' or 'minor'
+        all_scales = [ScaleMatchCriteria(self.cp.create_scale(root, scale_type), self.chords, self.cp)
+                      for root in NOTE_TO_TOKEN.keys()]
+        best_scale_criteria = max(all_scales, key=lambda k:k.score)
 
-        return best_scale
+        return best_scale_criteria.scale, best_scale_criteria.score
+    
+    def find_best_scale(self):
+        best_scale, score = self._find_best_scale()
+        other_criterias_score = sum([criteria_cls(best_scale, self.chords, self.cp).score for criteria_cls in self.CRITERIA_CLASSES])
+        return best_scale, score + other_criterias_score
 
 def test():
     # song = SongsChords('G B C Cm')
     song = SongsChords('C C G F C G Am Am F C C G F C G Am Am F C C G F C C G Am Am F Dm Em Bdim C C'.split())
-    best_scale = song.find_best_scale()
-    print(f'best scale is {best_scale}')
+    best_scale, score = song.find_best_scale()
+    print(f'best scale is {best_scale} with score {score}')
 
 if __name__ == '__main__':
     exit(test())
