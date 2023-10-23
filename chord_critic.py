5be09fad6b1270c4560db844eb3d50ef10f14e1c
Assaf Gadish <agadish@gmail.com>
2023-10-24 01:42:36 +0300

    Updated metrics

--- chord_critic.py
+++ chord_critic.py
@@ -1,11 +1,21 @@
 import chordparser
 from chord_tokenizer import NOTE_TO_TOKEN
+import numpy as np
 # from pychord import Chord
 # from pychord.constants import VAL_NOTE_DICT, NOTE_VAL_DICT
 # from pychord.constants.scales import RELATIVE_KEY_DICT
 
 
-class ChordsMatchCriteria(object):
+def find_all_sublist(target: list, pattern: list):
+    if len(pattern) > len(target):
+        return []
+    
+    all_occurences = [i for i in range(len(target) - len(pattern) + 1)
+                        if pattern == target[i : i + len(pattern)]]
+    return all_occurences
+
+
+class Criteria(object):
     def __init__(self, scale, chords, cp):
         self.scale = scale
         self.chords = chords
@@ -14,24 +24,52 @@ class ChordsMatchCriteria(object):
 
     def calc_score(self):
         raise NotImplementedError
+    
+    def __lt__(self, other):
+        return self.calc_score().__lt__(other.calc_score())
 
-class DiatomicMatchCriteria(ChordsMatchCriteria):
+    def __le__(self, other):
+        return self.calc_score().__le__(other.calc_score())
+
+    def __gt__(self, other):
+        return self.calc_score().__gt__(other.calc_score())
+
+    def __ge__(self, other):
+        return self.calc_score().__ge__(other.calc_score())
+    
+    def __eq__(self, other):
+        return self.calc_score().__eq__(other.calc_score())
+
+    def __ne__(self, other):
+        return self.calc_score().__ne__(other.calc_score())
+
+
+class InScaleCriteria(Criteria):
+    """
+    Percentage of chords that are degree in the diatonic scale
+    """
     def calc_score(self):
         diatonic_roles = [self.cp.analyse_diatonic(c, self.scale) for c in self.chords]
         matches_count = len([r for r in diatonic_roles if r])
         score = matches_count / len(self.chords)
         return score
     
-class BorrowedChordsMatchCriteria(ChordsMatchCriteria):
+class AdvancedScaleCriteria(Criteria):
+    """
+    Percentage of chords that are borrowed chords in the diatonic scale
+    """
     def calc_score(self):
-        diatonic_roles = [self.cp.analyse_all(c, self.scale) for c in self.chords]
-        matched_modes = [r for r in diatonic_roles
-                         if (r[1], self.scale.key.mode) in [('major', 'minor'), ('minor', 'major')]]
-        matches_count = len(matched_modes)
+        secondary_scale = self.cp.create_scale(self.scale.key.root, 'major' if self.scale.key.mode == 'minor' else 'minor')
+        secondary_roles = [self.cp.analyse_diatonic(c, secondary_scale) for c in self.chords]
+        matched_modes = [r for r in secondary_roles if r]
+        matches_count = len([m for m in matched_modes if m])
         score = matches_count / len(self.chords)
         return score
 
-class FirstChordMatchCriteria(ChordsMatchCriteria):
+class FirstScoreCriteria(Criteria):
+    """
+    Match between the first chord and the scale
+    """
     def calc_score(self):
         results = self.cp.analyse_diatonic(self.chords[0], self.scale)
         if results:  # diatonic
@@ -42,7 +80,7 @@ class FirstChordMatchCriteria(ChordsMatchCriteria):
                 return 0.5
         return 0
     
-class LastChordMatchCriteria(ChordsMatchCriteria):
+class LastScoreCriteria(Criteria):
     def calc_score(self):
         results = self.cp.analyse_diatonic(self.chords[-1], self.scale)
         if results:  # diatonic
@@ -51,15 +89,7 @@ class LastChordMatchCriteria(ChordsMatchCriteria):
                 return 1
         return 0
 
-def find_all_sublist(target: list, pattern: list):
-    if len(pattern) > len(target):
-        return []
-    
-    all_occurences = [i for i in range(len(target) - len(pattern) + 1)
-                        if pattern == target[i : i + len(pattern)]]
-    return all_occurences
-
-class ChordsProgressionsCoverageCriteria(ChordsMatchCriteria):
+class ChordsProgressionsCoverageCriteria(Criteria):
     """
     Appendix of "7" is not mandatory for match but increases the score
     """
@@ -134,23 +164,20 @@ class ChordsProgressionsCoverageCriteria(ChordsMatchCriteria):
         return best_used_map
                                 
 
-class RepetitionCriteria(ChordsMatchCriteria):
+class RepetitionCoverageCriteria(Criteria):
     MIN_REPETITION = 4
     def _find_repetition_mask(self):
-        tested_pairs = []
+        tested_pairs = {}
         occupied_mask = [False] * len(self.chords)
         l = self.MIN_REPETITION
-        for i in range(len(self.chords) - l * 2 + 1):
-            reps_indexes = [i for i in find_all_sublist(self.chords, self.chords[i : i + l])
-                            if (l, i, ) not in tested_pairs]
-            if len(reps_indexes) <= 1:
-                continue
-
-            for i in reps_indexes:
-                occupied_mask = occupied_mask[: i] + [True] * l + occupied_mask[i + l :]
-            # new_submask = self._find_repetition_mask(chords, cloned_mask, tested_pairs + [(l, i, ) for i in reps_indexes])
-            # if sum(new_submask) > sum(best_repetition_mask):
-                # best_repetition_mask = new_submask
+        chords_tuple = tuple([str(chord) for chord in self.chords])
+        for i in range(len(self.chords) - l + 1):
+            curr = chords_tuple[i : i + l]
+            reps_indexes = tested_pairs.setdefault(curr, list())
+            reps_indexes.append(i)
+            if len(reps_indexes) > 1:
+                for i in reps_indexes:
+                    occupied_mask = occupied_mask[: i] + [True] * l + occupied_mask[i + l :]
         
         return occupied_mask
     
@@ -159,94 +186,76 @@ class RepetitionCriteria(ChordsMatchCriteria):
         score = sum(best_mask) / len(self.chords)
         return score
     
-class ScaleMatchCriteria(ChordsMatchCriteria):
-    CRITERIAS_CLASSES = [DiatomicMatchCriteria, FirstChordMatchCriteria, LastChordMatchCriteria, ChordsProgressionsCoverageCriteria]
+    
+class LocalScaleCriteria(Criteria):
+    """
+    Scale criteria for a specific scale, not necessarily the best
+    """
     def calc_score(self):
-        critereas_scores = [criteria_cls(self.scale, self.chords, self.cp).score
-                            for criteria_cls in self.CRITERIAS_CLASSES]
-        return sum(critereas_scores)
+        self.first_score = FirstScoreCriteria(self.scale, self.chords, self.cp).score
+        self.last_score = LastScoreCriteria(self.scale, self.chords, self.cp).score
+        self.inscale_score = InScaleCriteria(self.scale, self.chords, self.cp).score
+        self.advscale_score = AdvancedScaleCriteria(self.scale, self.chords, self.cp).score
+        total = self.first_score + self.last_score + (1.5 * self.inscale_score) + (0.5 * self.advscale_score) + 1
+        return total
     
-# class ScaleMatch(object):
-#     def __init__(self, scale, chords, cp, criterias=None):
-#         if not criterias:
-#             criterias = list()
-#         self.criterias = [
-#             RepetitionCriteria(scale, chords, cp)
-#         ]
-#         self.scale = scale
-#         self.chords = chords
-#         self.cp = cp
-#         self.score = sum([criteria.score for criteria in self.criterias])
 
-#     def __lt__(self, other):
-#         if self.score != other.score:
-#             return self.score < other.score
-        
-#         if self.chords[0].root != self.scale.root:
-#             if self.chords[0].root == self.chords[0].root:
-#                 return False
-#             elif self.scale.root == self.chords[0].root:
-#                 return True
-   
-#         # Equal
-#         return False
-    
-#     def __le__(self, other):
-#         if self.score != other.score:
-#             return self.score <= other.score
-        
-#         if self.chords[0].root != self.scale.root:
-#             if self.chords[0].root == self.chords[0].root:
-#                 return False
-#             elif self.scale.root == self.chords[0].root:
-#                 return True
-   
-#         # Equal
-#         return True
-        
-#     def __gt__(self, other):
-#         if self.score != other.score:
-#             return self.score > other.score
-        
-#         if self.chords[0].root != self.scale.key.root:
-#             if self.chords[0].root == self.chords[0].root:
-#                 return True
-#             elif self.scale.key.root == self.chords[0].root:
-#                 return False
-   
-#         # Equal
-#         return False
-        
-#     def __ge__(self, other):
-#         if self.score != other.score:
-#             return self.score > other.score
-        
-#         if self.chords[0].root != self.scale.root:
-#             if self.chords[0].root == self.chords[0].root:
-#                 return True
-#             elif self.scale.root == self.chords[0].root:
-#                 return False
-   
-#         # Equal
-#         return True
-    
-#     def __cmp__(self, other):
-#         return self.scale == other._scale and self.chords == other._chords
-    
-#     def __str__(self):
-#         return f'[ScaleMatch: {self.scale} score={self.score}]'
-    
-#     def __repr__(self):
-#         return self.__str__()
+class ScaleCriteria(Criteria):
+    def __init__(self, scale, chords, cp):
+        super(ScaleCriteria, self).__init__(scale, chords, cp)
+        self._best_scale_criteria = None
 
-class SongsChords(object):
-    CRITERIA_CLASSES = [
-        RepetitionCriteria
-    ]
+    @property
+    def best_scale_criteria(self):
+        if self._best_scale_criteria is None:
+            self.calc_score()
+        return self._best_scale_criteria
+    
+    def _get_all_scales_criteria(self):
+        # The scale will be major/minor depending on the type of the first note
+        all_major_scales = [LocalScaleCriteria(self.cp.create_scale(root, 'major'), self.chords, self.cp)
+                            for root in NOTE_TO_TOKEN.keys()]
+        all_minor_scales = [LocalScaleCriteria(self.cp.create_scale(root, 'minor'), self.chords, self.cp)
+                            for root in NOTE_TO_TOKEN.keys()]
+        all_scales = all_major_scales + all_minor_scales
+
+        return all_scales
+
+    def calc_score(self):
+        all_criterias = self._get_all_scales_criteria()
+        best_criteria = max(all_criterias, key=lambda c:c.score)
+        # Softmax
+        score = np.exp(best_criteria.score) / np.sum(np.exp([c.score for c in all_criterias]))
+        self._best_scale_criteria = best_criteria
+        return score
+        
+
+class ArtCriteria(Criteria):
+    def __init__(self, best_scale, chords, cp, best_scale_criteria):
+        self.best_scale_criteria = best_scale_criteria
+        super(ArtCriteria, self).__init__(best_scale, chords, cp)
+
+    def calc_score(self):
+        best_scale_criteria = self.best_scale_criteria
+        in_scale = best_scale_criteria.inscale_score
+        adv_scale = best_scale_criteria.advscale_score
+        result = 2 * (adv_scale + 1) / (in_scale + 1)
+        return result
+
+class StructureCriteria(Criteria):
+    def calc_score(self):
+        chord_progression_coverage = ChordsProgressionsCoverageCriteria(self.scale, self.chords, self.cp)
+        repetition_coverage = RepetitionCoverageCriteria(self.scale, self.chords, self.cp)
+        return np.sqrt(chord_progression_coverage.score * repetition_coverage.score)
+
+class ChordsMetrics(object):
     def __init__(self, chords: list):
         self.cp = chordparser.Parser()
         self._dedup_raw_chords = self._remove_dupliate_chords(chords)
         self.chords = [self.cp.create_chord(c) for c in self._dedup_raw_chords]
+        self._scale_criteria = ScaleCriteria(None, self.chords, self.cp)
+        self._art_criteria = ArtCriteria(self.best_scale_criteria.scale, self.chords, self.cp, self.best_scale_criteria)
+        self._structure_criteria = StructureCriteria(self.best_scale_criteria.scale, self.chords, self.cp)
 
     @classmethod
     def _remove_dupliate_chords(cls, chords):
@@ -255,31 +264,34 @@ class SongsChords(object):
             if chord != new_chords[-1]:
                 new_chords.append(chord)
         return new_chords
-
-    def _find_best_scale(self):
-        # The scale will be major/minor depending on the type of the first note
-        best_score = 0
-        best_scale = None
-        scale_type = self.chords[0].quality.value # 'major' or 'minor'
-        all_scales = [ScaleMatchCriteria(self.cp.create_scale(root, scale_type), self.chords, self.cp)
-                      for root in NOTE_TO_TOKEN.keys()]
-        best_scale_criteria = max(all_scales, key=lambda k:k.score)
-
-        return best_scale_criteria.scale, best_scale_criteria.score
     
-    def find_best_scale(self):
-        best_scale, score = self._find_best_scale()
-        other_criterias_score = sum([criteria_cls(best_scale, self.chords, self.cp).score for criteria_cls in self.CRITERIA_CLASSES])
-        return best_scale, score + other_criterias_score
-    
-    def calc_score(self):
-        return self.find_best_scale()[1]
+    @property
+    def best_scale_criteria(self):
+        return self._scale_criteria.best_scale_criteria
 
+    @property
+    def scale_score(self):
+        return self._scale_criteria.score
+        
+    @property
+    def art_score(self):
+        return self._art_criteria.score
+    
+    @property
+    def structure_score(self):
+        return self._structure_criteria.score
+    
+    @property
+    def scores(self):
+        return self.scale_score, self.art_score, self.structure_score
+
+    
 def test():
     # song = SongsChords('G B C Cm')
-    song = SongsChords('C C G F C G Am Am F C C G F C G Am Am F C C G F C C G Am Am F Dm Em Bdim C C'.split())
-    best_scale, score = song.find_best_scale()
-    print(f'best scale is {best_scale} with score {score}')
+    metrics = ChordsMetrics('C C G F C G Am Am F C C G F C G Am Am F C C G F C C G Am Am F Dm Em Bdim C C F# A'.split())
+    best_scale = metrics.best_scale_criteria.scale
+    scores = metrics.scores
+    print(f'best scale is {best_scale} with scores {scores}')
 
 if __name__ == '__main__':
     exit(test())
